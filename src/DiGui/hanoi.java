5e424a7add50e1e2d982e1ab6c30c974940c0409
yufeng <1715024080@qq.com>
2019-11-22 13:51:17 +0800

    NIO初学之Nio

--- src/DiGui/hanoi.java
+++ src/DiGui/hanoi.java
@@ -10,10 +10,29 @@ public class hanoi {
 
     public static void main(String[] args) {
         while (true) {
+            char a='a';
+            char b='b';
+            char c='c';
+
             Scanner scanner = new Scanner(System.in);
             System.out.println("请输入n:");
             int n = scanner.nextInt();
-            System.out.println(Math.pow(2, n - 1));
+            move(n,a,b,c);
         }
     }
+
+    private static void move(int n, char a, char b, char c) {
+        if (1 == n){
+            System.out.println(a + "-->" + c);
+        }else {
+            //将n-1个盘子从a借助c移到b上
+            move(n-1,a,c,b);
+            //将第n个盘子从a移到c上
+            System.out.println(a + "-->" + c);
+            //将n-1个盘子从b借助a移到c上
+            move(n-1,b,a,c);
+        }
+    }
+
+
 }
